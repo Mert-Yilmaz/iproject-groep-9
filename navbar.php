20f48efaec46f34eac6498d68bfc497ea91ae9d9
JeffreyKragten <35327042+JeffreyKragten@users.noreply.github.com>
2018-04-24 14:24:39 +0200

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,24 @@
+<div class="grid-container">
+  <div class="grid-x grid-padding-x">
+    <div class="large-6 text-center cell">
+      <a><font size= "30px">EenmaalAndermaal</font></a>
+    </div>
+
+    <div class="large-6 text-center cell">
+      <div class="grid-x grid-padding-x">
+        <div class="large-3 medium-3 cell">
+          <a href= "producten.php" class="menuu">Items</a>
+        </div>
+        <div class="large-3 medium-3 cell">
+          <a href= "bod.php" class="menuu">Bied aan</a>
+        </div>
+        <div class="large-3 medium-3 cell">
+          <a href="account.php" class="menuu">Account</a>
+        </div>
+        <div class="large-3 medium-3 cell">
+          <a href="login.php" class="menuu">Over EA</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
