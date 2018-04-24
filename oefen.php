03ca3c69f78ab6937ee0318ded9f27383032a319
JeffreyKragten <35327042+JeffreyKragten@users.noreply.github.com>
2018-04-24 14:13:22 +0200

    Begin van de website incl Foundation
    Plus het onderzoeksrapport, schermontwerp en sitecomponenten.

---
+++ oefen.php
@@ -0,0 +1,80 @@
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Oefensite Foundation</title>
+  <link rel="stylesheet" href="css/foundation.css">
+  <link rel="stylesheet" href="css/app.css">
+</head>
+
+<div class="grid-x">
+  <div class="cell small-6 large-4 columns" style="background: lightblue">hallo wereld
+grid-padding-xd
+grid-padding-xd
+d onloadedmetadata="d
+
+
+ dddwef
+ wefwe
+fwefwe
+wefwf
+wfew"</div>
+  <div class="cell small-6 large-8 columns" style="background: green">haeld</div>
+</div>
+
+<div class="grid-x">
+  <div class="cell small-6 large-2 columns" style="background: yellow">Left menu</div>
+  <div class="cell small-6 large-8 large-offset-2 columns" style="background: orange">Right menu</div>
+</div>
+
+<form action="http://www.google.com" class="show-for-large">
+  <button class="Alert button hollow">Klik!</button>
+</form>
+
+<a href="http://www.google.com" class="button">Link</a>
+
+<table class="hover stack">
+  <tr>
+    <th>Rij 1</th>
+    <th>Rij 2</th>
+    <th>Rij 3</th>
+  </tr>
+  <tr>
+    <td>Dit</td>
+    <td>Dat</td>
+    <td>Etc</td>
+  </tr>
+  <tr>
+    <td>Dit</td>
+    <td>Dat</td>
+    <td>Etc</td>
+  </tr>
+  <tr>
+    <td>Dit</td>
+    <td>Dat</td>
+    <td>Etc</td>
+  </tr>
+  <tr>
+    <td>Dit</td>
+    <td>Dat</td>
+    <td>Etc</td>
+  </tr>
+</table>
+
+
+<div class="grid-x">
+  <div class="cell large-10 columns">
+    <h3>Hoofdtekst</h3>
+    <p>waaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+  </div>
+  <div class="cell large-2 columns">
+    <h3>Sidebar</h3>
+    <p>waaaaaaaaaaaa
+      aaaaaaaaaaaaaaaaa
+      aaaaaaaaaaaaaaa</p>
+  </div>
+</div>
+
+
+
+<button class="success button expanded">Vet brede knop jonge!</button>
