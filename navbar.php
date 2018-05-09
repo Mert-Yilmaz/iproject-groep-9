84b011c51055689bca80d0a37c16f94761ffa0d9
MNLBHAN <38658425+MNLBHAN@users.noreply.github.com>
2018-05-09 10:43:03 +0200

    Add files via upload

--- navbar.php
+++ navbar.php
@@ -1,36 +1,38 @@
-<div class="grid-container">
-  <div class="grid-x grid-padding-x">
-    <div class="large-6 text-center cell">
-      <a href="index.php"><font size= "30px">EenmaalAndermaal</font></a>
-    </div>
+<div class="navbar">
+  <div class="grid-container">
+    <div class="grid-x grid-padding-x">
+      <div class="large-4 text-center cell">
+      <a href="index.php"><img src="img/eenmaalandermaal.svg" alt="EenmaalAndermaal" width="40%" height="30%"></a>
+      </div>
 
-    <div class="large-6 text-center cell">
-      <div class="grid-x grid-padding-x">
-        <div class="large-3 medium-3 cell">
-          <div class="grid-y grid-padding-y">
-            <div class="cell small-6">
-              <a href= "producten.php">Items</a>
+      <div class="large-8 text-center cell">
+        <div class="grid-x grid-padding-x">
+          <div class="large-3 medium-3 cell">
+            <div class="grid-y grid-padding-y">
+              <div class="cell small-6">
+                <a href= "index.php">HOME</a>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="large-3 medium-3 cell">
-          <div class="grid-y grid-padding-y">
-            <div class="cell small-6">
-              <a href= "bod.php" class="menuu">Bied aan</a>
+          <div class="large-3 medium-3 cell">
+            <div class="grid-y grid-padding-y">
+              <div class="cell small-6">
+                <a href= "account.php">ACCOUNT</a>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="large-3 medium-3 cell">
-          <div class="grid-y grid-padding-y">
-            <div class="cell small-6 medium-8 large-2">
-              <a href="account.php" class="menuu">Account</a>
+          <div class="large-3 medium-3 cell">
+            <div class="grid-y grid-padding-y">
+              <div class="cell small-6 medium-8 large-2">
+                <a href="bod.php">PLAATS ADVERTENTIE</a>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="large-3 medium-3 cell">
-          <div class="grid-y grid-padding-y">
-            <div class="cell small-6">
-              <a href="about.php" class="menuu">Over EA</a>
+          <div class="large-3 medium-3 cell">
+            <div class="grid-y grid-padding-y">
+              <div class="cell small-6">
+                <a href="about.php">ABOUT</a>
+              </div>
             </div>
           </div>
         </div>
