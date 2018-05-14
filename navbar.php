938944c4b876388c0f75c547698bc32511bc4a8e
MNLBHAN <38658425+MNLBHAN@users.noreply.github.com>
2018-05-14 09:41:06 +0200

    ja

---
+++ navbar.php
@@ -0,0 +1,42 @@
+<div class="navbar">
+  <div class="grid-container">
+    <div class="grid-x grid-y grid-padding-x">
+      <div class="large-4 text-center cell">
+      <a href="index.php"><img src="img/eennaalandermaaal.svg" alt="EenmaalAndermaal" width="40%" height="30%"></a>
+      </div>
+
+      <div class="large-8 text-center cell">
+        <div class="grid-x grid-padding-x">
+          <div class="large-3 medium-3 cell">
+            <div class="grid-y grid-padding-y">
+              <div class="cell small-6">
+                <a href= "index.php">HOME</a>
+              </div>
+            </div>
+          </div>
+          <div class="large-3 medium-3 cell">
+            <div class="grid-y grid-padding-y">
+              <div class="cell small-6">
+                <a href= "account.php">ACCOUNT</a>
+              </div>
+            </div>
+          </div>
+          <div class="large-3 medium-3 cell">
+            <div class="grid-y grid-padding-y">
+              <div class="cell small-6 medium-8 large-2">
+                <a href="bod.php">VERKOPEN</a>
+              </div>
+            </div>
+          </div>
+          <div class="large-3 medium-3 cell">
+            <div class="grid-y grid-padding-y">
+              <div class="cell small-6">
+                <a href="about.php">OVER</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
