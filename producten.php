2dd388abc04f7dc9022e30137a457f90e79681fc
Mert-Yilmaz <36151182+Mert-Yilmaz@users.noreply.github.com>
2018-05-15 10:05:46 +0200

    fixed

--- producten.php
+++ producten.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>De beste veilingsite van Nederland!</title>
     <link rel="stylesheet" href="css/foundation.css">
-    <link rel="stylesheet" href="css/app.csss">
+    <link rel="stylesheet" href="css/app.css">
   </head>
 <body>
 
