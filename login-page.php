6ebaab0c0943f4a55222299796f5fd183f5c43a7
MNLBHAN <38658425+MNLBHAN@users.noreply.github.com>
2018-05-16 14:31:54 +0200

    dingen

--- login-page.php
+++ login-page.php
@@ -30,35 +30,11 @@ if(isset($_SESSION['login_token'])) {
         <link rel="icon" href="img/logo2.PNG">
     </head>
 
-    <!-- DIT IN STYLESHEET -->
-    <style>
-        .login-container {
-            width: 50%;
-            margin: 2% auto 0 auto;
-        }
-
-        .login-container img {
-            display: block;
-            margin: 0 auto;
-        }
-
-        .login-container input[type=submit] {
-            width: 75%;
-            margin: 0 12.5%;
-        }
-
-        .login-container p {
-            text-align: center;
-            margin-top: 1%;
-        }
-    </style>
-
     <body>
         <div class="login-container">
-            <img src="img/Eennaalandermaaal.svg" alt="EenmaalAndermaal" width="45%">
             <form method="POST" action="login.php">
                 <input type="text" name="username" id="username" placeholder="Gebruikersnaam (of email adres, nog niet!)" required>
-                <input type="password" name="password" id="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}" placeholder="Wachtwoord" required>
+                <input type="password" name="password" id="password" placeholder="Wachtwoord" required>
                 <input type="submit" name="loginbtn" value="Log in">
             </form>
             <p>Heb je nog geen account? <a href="account.php">Maak een account aan en bied mee!</a></p>
