fee6bc1c31de2f58547e1921e744cebfbfd7f443
JeffreyKragten <35327042+JeffreyKragten@users.noreply.github.com>
2018-05-16 10:34:07 +0200

    index?

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<!doctype html>
->>>>>>> 3cf35405ccf745598c768b274745f24448df97a0
 <?php include 'navbar.php';
       include 'functions.php';
       include_once 'db.php';
@@ -12,29 +8,14 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-    <title>De beste veilingsite van Nederland!</title>
-    <link rel="stylesheet" href="css/foundation.css">
-    <link rel="stylesheet" href="css/app.css">
-=======
     <title>EenmaalAndermaal - De beste veilingsite van Nederland!</title>
     <link rel="stylesheet" href="css/foundation.css">
     <link rel="stylesheet" href="css/app.css">
     <link rel="icon" href="img/logo2.PNG">
->>>>>>> 3cf35405ccf745598c768b274745f24448df97a0
   </head>
   <body>
     <div class="grid-container fluid">
       <div class="grid-x grid-padding-x">
-<<<<<<< HEAD
-        <div class="cell large-4 text-center">
-          <div class="grid-y">
-            <div class="cell small-6 medium-8 large-2">
-              <form method="post" action="">
-                    <input type="text" name="zoekterm"/>
-                    <input type="submit" name="zoeken" />
-              </form>
-=======
         <div class="cell large-2 text-center">
           <div class="grid-y">
             <div class="cell small-6 medium-8 large-2">
@@ -45,33 +26,12 @@
               </form>
                 <br>
                 <h1>Groepen</h1>
->>>>>>> 3cf35405ccf745598c768b274745f24448df97a0
               <?php
                 echo '<ul class="ulNone">';
                     zoekRubriek($dbh, $_POST['zoekterm']);
                 echo '</ul>';
               ?>
             </div>
-<<<<<<< HEAD
-            <div class="cell small-6 medium-4 large-10">
-              <p>Hier komt geen tekst.</p>
-            </div>
-          </div>
-        </div>
-        <div class="cell large-8 text-center">
-          <?php include 'orbit-slider.php'; ?>
-        </div>
-      </div>
-      <div class="grid-x grid-padding-x">
-        <div class="small-12 cell">
-          <div class="callout">
-            <h3 class="text-center">Welkom bij de beste veilingsite van Nederland!</h3>
-            <p class="text-center">Bekijk hieronder populaire items of klik op een rubriek voor nog meer leuke koopjes!</p>
-            <div class="grid-x grid-padding-x">
-              <?php hot_items($dbh); ?>
-            </div>
-          </div>
-=======
           </div>
         </div>
         <div class="cell large-8 text-center">
@@ -100,7 +60,6 @@
             </div>
           </div>
           <?php include 'orbit-slider.php'; ?>
->>>>>>> 3cf35405ccf745598c768b274745f24448df97a0
         </div>
       </div>
     </div>
@@ -109,16 +68,8 @@
     <script src="js/vendor/what-input.js"></script>
     <script src="js/vendor/foundation.js"></script>
     <script src="js/app.js"></script>
-<<<<<<< HEAD
-
-      <?php
-      include("footer.html");
-    ?>
-
-=======
     <?php
     include_once 'footer.html';
     ?>
->>>>>>> 3cf35405ccf745598c768b274745f24448df97a0
   </body>
 </html>
