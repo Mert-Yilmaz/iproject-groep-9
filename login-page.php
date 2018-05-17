dd0b77f9df1d14376d2ebabc35b5c3539ac3153f
Demivk <38658490+Demivk@users.noreply.github.com>
2018-05-17 14:52:24 +0200

    Update login-page.php

--- login-page.php
+++ login-page.php
@@ -23,26 +23,15 @@ if (isset($_SESSION['login_token'])) {
 </head>
 
 <body>
-<?php
-
-include 'navbar.php';
-?>
+<?php include 'navbar.php'; ?>
 <div class="login-container">
     <form method="POST" action="login.php">
         <input type="text" name="username" id="username" placeholder="Gebruikersnaam (of email adres, nog niet!)" required>
         <input type="password" name="password" id="password" placeholder="Wachtwoord" required>
         <input type="submit" class="knop" name="loginbtn" value="Log in">
-        <?php include 'navbar.php'; ?>
-
-        <div class="login-container">
-            <img src="img/Eennaalandermaaal.svg" alt="EenmaalAndermaal" width="45%">
-            <form method="POST" action="login.php">
-                <input type="text" name="username-mail" id="username-mail" placeholder="Gebruikersnaam of email adres" required>
-                <input type="password" name="password" id="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}" placeholder="Wachtwoord" required>
-                <input type="submit" name="loginbtn" value="Log in">
-            </form>
-            <p>Heb je nog geen account? <a href="register.php">Maak een account aan en bied mee!</a></p>
-        </div>
-        <?php include_once 'footer.html' ?>
+    </form>
+    <p>Heb je nog geen account? <a href="register.php">Maak een account aan en bied mee!</a></p>
+</div>
+<?php include_once 'footer.html' ?>
 </body>
 </html>
