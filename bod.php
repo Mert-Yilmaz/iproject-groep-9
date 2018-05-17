ba252e7e284bfb1457fc8193e085388a46de9261
Demivk <38658490+Demivk@users.noreply.github.com>
2018-05-17 14:54:11 +0200

    Update bod.php

--- bod.php
+++ bod.php
@@ -1,27 +1,28 @@
 <!doctype html>
 <?php include 'navbar.php';
-      include 'functions.php';
-      include_once 'db.php';
-      error_reporting(E_ALL ^ E_NOTICE);
+include 'functions.php';
+include_once 'db.php';
+error_reporting(E_ALL ^ E_NOTICE);
 ?>
 <html class="no-js" lang="en" dir="ltr">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EenmaalAndermaal - De beste veilingsite van Nederland!</title>
     <link rel="stylesheet" href="css/foundation.css">
     <link rel="stylesheet" href="css/app.css">
-  </head>
-  <body>
+</head>
+<body>
 
-  <p class='text-center'>Deze pagina wordt nog gemaakt.</p>
+<p class='text-center'>Deze pagina wordt nog gemaakt.</p>
 
-    <?php
-      for ($i = 1; $i < 20; $i++){
-          echo '<img src="img/spooky.gif">';
-          }
-      include("footer.html");
-    ?>
+<?php
+for ($i = 1; $i < 20; $i++) {
+    echo '<img src="img/spooky.gif">';
+}
+include("footer.html");
+?>
 
 </body>
+</html>
