642e866fee6609bd895fbfe8563257f88958df8d
Demivk <38658490+Demivk@users.noreply.github.com>
2018-05-18 11:54:11 +0200

    Update login-page.php

--- login-page.php
+++ login-page.php
@@ -26,8 +26,8 @@ error_reporting(E_ALL ^ E_NOTICE);
 <?php include 'navbar.php'; ?>
 <div class="login-container">
     <form method="POST" action="login.php">
-        <input type="text" name="username-mail" id="username-mail" placeholder="Gebruikersnaam of email adres" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}">
-        <input type="password" name="password" id="password" placeholder="Wachtwoord" required>
+        <input type="text" name="username-mail" id="username-mail" placeholder="Gebruikersnaam of email adres" required>
+        <input type="password" name="password" id="password" placeholder="Wachtwoord" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}">
         <input type="submit" class="knop" name="loginbtn" value="Log in">
     </form>
     <p>Heb je nog geen account? <a href="register.php">Maak een account aan en bied mee!</a></p>
