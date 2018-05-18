914580cc6c7f21fd77acb261688914d38cef7b76
Demivk <38658490+Demivk@users.noreply.github.com>
2018-05-18 11:49:51 +0200

    Update login.php

--- login.php
+++ login.php
@@ -7,20 +7,22 @@ if(isset($_POST['username-mail']) AND isset($_POST['password'])) {
     $usernamemail = $_POST['username-mail'];
     $passworduser = $_POST['password'];
 
-    $query_login = "SELECT * FROM Gebruiker WHERE (gebruikersnaam='$usernamemail' OR mailbox='$usernamemail') AND wachtwoord='$passworduser'";
-    $result = $dbh->query($query_login);
-    $count = $result->rowCount();
+    if(isset($_POST['loginbtn'])) {
+        $query_login = "SELECT * FROM Gebruiker WHERE (gebruikersnaam='$usernamemail' OR mailbox='$usernamemail') AND wachtwoord='$passworduser'";
+        $result = $dbh->query($query_login);
+        $count = $result->rowCount();
 
-    if($count == 1 || $count == -1) {
-        $_SESSION['login-token'] = $usernamemail;
-        header("Location: index.php");
-    } else {
-        header("Location: login-page.php");
+        if ($count == 1 || $count == -1) {
+            $_SESSION['login-token'] = $usernamemail;
+            header("Location: index.php");
+        } else {
+            header("Location: login-page.php");
+        }
     }
 }
 
-if(isset($_SESSION['username-mail'])) {
-    $usernamemail = $_SESSION['username-mail'];
-    echo "Hi " . $usernamemail;
-}
+//if(isset($_SESSION['username-mail'])) {
+//    $usernamemail = $_SESSION['username-mail'];
+//    echo "Hi " . $usernamemail;
+//}
 ?>
