11f572ea5d9b8d8889b9d10d40149c73e649dcb8
MNLBHAN <38658425+MNLBHAN@users.noreply.github.com>
2018-05-22 11:08:08 +0200

    jup

--- personpage.php
+++ personpage.php
@@ -12,27 +12,38 @@ error_reporting(E_ALL ^ E_NOTICE);
   <title>EenmaalAndermaal - De beste veilingsite van Nederland!</title>
   <link rel="stylesheet" href="css/foundation.css">
   <link rel="stylesheet" href="css/app.css">
-  <link rel="icon" href="img/logo2.PNG">
 </head>
 <body>
-
-<h1>Hallo,  {naam}</h1>
-<h2>Dit zijn uw NAW-gegevens</h2>
-
-<hr>
-
-<p>emailadres $_POST['emailadres']</p>
-<p>Wachtwoord</p>
-<p>telefoonnummer<p>
-<p>adres</p>
-<p>plaats<p>
-<p>Postcode<p>
-<p>Land</p>
-<p>gekozen beveiligingsvraag<p>
-
-
-        <?php
-        include_once 'footer.html';
-        ?>
-      </body>
-      </html>
+  <div class="grid-container">
+    <div class="grid-y">
+      <div class="cell">
+        <h1 class="aboutkop text-center">Hallo,  {naam}</h1>
+        <h2>Dit zijn uw NAW-gegevens</h2>
+        <p>emailadres $_POST['emailadres']</p>
+        <p>Wachtwoord</p>
+        <p>telefoonnummer<p>
+        <p>adres</p>
+        <p>plaats<p>
+        <p>Postcode<p>
+        <p>Land</p>
+        <p>gekozen beveiligingsvraag</p>
+        <p>verkoper ja nee</p>
+      </div>
+    </div>
+    <div class="cell">
+      <h1 class="aboutkop">Privacybeleid</h1>
+      <h2>Belangrijkste hoofdpunten waar u mee instemt</h2>
+      <p>Door de service te gebruiken of ermee om te gaan, stemt u in met:</P>
+      <ul>
+        <li>het gebruik van cookies en andere technologieën.</li>
+        <li>Het verzamelen, gebruiken, delen en op andere manier verwerken van uw informatie.</li>
+        <li>De openbare beschikbaarheid van uw informatie en de controles over dergelijke informatie, zoals beschreven in Het delen van informatie.</li>
+        <li>In het geval van een fusie worden persoonsgegevens overgedragen naar het bedrijf in kwestie.</li>
+      </ul>
+    </div>
+  </div>
+    <?php
+    include("footer.html");
+    ?>
+  </body>
+</html>
