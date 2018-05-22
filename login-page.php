be25eb387eb83cc864ffe4ccf4b4900e8d788ec4
Demivk <38658490+Demivk@users.noreply.github.com>
2018-05-22 09:50:37 +0200

    Update login-page.php

--- login-page.php
+++ login-page.php
@@ -28,6 +28,12 @@ error_reporting(E_ALL ^ E_NOTICE);
     <form method="POST" action="login.php">
         <input type="text" name="username-mail" id="username-mail" placeholder="Gebruikersnaam of email adres" required>
         <input type="password" name="password" id="password" placeholder="Wachtwoord" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}">
+        <?php
+        if (isset($_SESSION['errormessage'])) {
+            echo $_SESSION['errormessage'];
+            unset($_SESSION['errormessage']);
+        }
+        ?>
         <input type="submit" class="knop" name="loginbtn" value="Log in">
     </form>
     <p>Heb je nog geen account? <a href="register.php">Maak een account aan en bied mee!</a></p>
