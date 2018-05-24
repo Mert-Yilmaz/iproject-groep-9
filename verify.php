afe385b48f80d750f17a97aa2c26e48299d47e7c
Demivk <38658490+Demivk@users.noreply.github.com>
2018-05-24 17:31:50 +0200

    Update verify.php

--- verify.php
+++ verify.php
@@ -3,41 +3,49 @@
  * Created by PhpStorm.
  * User: demiv
  * Date: 24-5-2018
- * Time: 11:07
+ * Time: 11:21
  */
-
-if(isset($_POST['email']) && isset($_POST['code'])) {
-    $email = $_POST['email'];
-    $code = $_POST['code'];
-
-    $sql = $dbh->prepare("SELECT email, password FROM Gebruiker WHERE email='$email' AND code='$code'");
-    $sql->execute();
-    $match = sqlsrv_num_rows($sql);
-    if($match > 0) {
-        sqlsrv_query("UPDATE Gebruiker SET actief='1' WHERE email='$email' AND code='$code'");
-    }
-}
 
 include 'functions.php';
 include_once 'db.php';
 error_reporting(E_ALL ^ E_NOTICE);
+
+if(isset($_POST['register'])) {
+    $email = $_POST['email'];
+    $code = md5(rand(0,1000));
+
+    //Uit te voeren query
+    $insert = sqlsrv_query("UPDATE Gebruiker SET code='$code' WHERE mailbox='$email'");
+
+    $to = $email;
+    $from = "noreply@eenmaalandermaal9.nl";
+    $subject = "Verificatie account EenmaalAndermaal (groep 9)";
+    $message = 'Bedankt voor je registratie bij EenmaalAndermaal! Je hebt de onderstaande gegevens ingevuld:
+    ---------------
+    Gebruikersnaam: HIER KOMT DE USERNAME
+    Email adres:' . $email . '
+    ?Wachtwoord: HIER KOMT HET WACHTWOORD?
+    ---------------
+    
+    Klik op de onderstaande link om je account te activeren en mee te bieden:
+    <a href="verification.php">verify.php?email=' . $email . '&code=' . $code.'</a>';
+    $headers = "From: " . $from;
+    mail($to, $subject, $message, $headers);
+    echo 'Er is een mail gestuurd naar' . $email . 'met een activatiecode.';
+}
+
+if(isset($_GET['email']) && isset($_GET['code'])) {
+    $email = $_GET['email'];
+    $code = $_GET['code'];
+
+    //Uit te voeren query:
+    $select = sqlsrv_query("SELECT email, code FROM Gebruiker WHERE email='$email' AND code='$code'");
+    if(sqlsrv_num_rows($select) > 0) {
+        //Uit te voeren query:
+        $updateActief = sqlsrv_query("UPDATE Gebruiker SET actief=1 WHERE mailbox='$email'");
+        //header('Location: index.php');
+        echo '<script type="text/javascript">alert("Registratie voltooid! Log in en start met bieden!")</script>';
+    }
+}
+
 ?>
-<!doctype html>
-<html class="no-js" lang="en" dir="ltr">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EenmaalAndermaal - Verify</title>
-    <link rel="stylesheet" href="css/foundation.css">
-    <link rel="stylesheet" href="css/app.css">
-    <link rel="icon" href="img/logo2.PNG">
-</head>
-<body>
-<?php include 'navbar.php'; ?>
-
-<h1>Bedankt voor het aanmelden! Check je mailbox voor de activatiecode!</h1>
-
-<?php include_once 'footer.html'; ?>
-</body>
-</html>
