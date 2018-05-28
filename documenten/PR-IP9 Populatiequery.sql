5eefce55a79b2efce356d59252118637d9a07fcf
Mert Yilmaz <mert.yilmaz@live.nl>
2018-05-28 15:16:41 +0200

    Added testitems

--- documenten/PR-IP9 Populatiequery.sql
+++ documenten/PR-IP9 Populatiequery.sql
@@ -3512,7 +3512,12 @@ VALUES	(0000000001, 'BMW 340i',		'Mooie BMW',						50.00,	'IDeal/PayPal',	'Gewoo
 		(0000000005, 'BMW 535i',		'Fijne auto',						85.00,	'IDeal/PayPal', 'Contant betalen kan niet',		'Arnhem',	'Nederland',	7,	'27-may-2018',	'09:20:20.0000000', null,	null,	'Admin',	'Gebruiker1',	'29-may-2018',	'20:20:21', 1,	90.00),
 		(0000000006, 'Audi RS6',		'Lekkere motor',					70.00,	'IDeal/PayPal', 'Kijk maar of je betaald',		'Arnhem',	'Nederland',	7,	'28-may-2018',	'09:20:20.0000000', null,	null,	'Admin',	'Gebruiker1',	'29-may-2018',	'20:20:21', 1,	80.00),
 		(0000000007, 'Renault Megane',	'Lelijke auto',						10.00,	'IDeal/PayPal', 'Kijk maar of je betaald',		'Boxmeer',	'Nederland',	3,	'28-may-2018',	'09:20:20.0000000', null,	null,	'Admin',	'Gebruiker1',	'29-may-2018',	'20:20:21', 1,	15.00),
-		(0000000008, 'Mercedes C63s',	'Heeft geen smaak',					270.00, 'IDeal/PayPal', 'Betaal gewoon kill',			'Arnhem',	'Nederland',	7,	'28-may-2018',	'09:20:20.0000000', null,	null,	'Admin',	'Gebruiker1',	'29-may-2018',	'20:20:21', 1,	280.00);
+		(0000000008, 'Mercedes C63s',	'Heeft geen smaak',					270.00, 'IDeal/PayPal', 'Betaal gewoon kill',			'Arnhem',	'Nederland',	7,	'28-may-2018',	'09:20:20.0000000', null,	null,	'Admin',	'Gebruiker1',	'29-may-2018',	'20:20:21', 1,	280.00),
+		(0000000014, 'Call Of Duty BO3','Gekke gimma a niffaauwwww',		40.00,	'IDeal/PayPal', 'Betaal gewoon kill',			'Arnhem',	'Nederland',	7,	'28-may-2018',	'09:20:20.0000000', null,	null,	'Admin',	'Gebruiker1',	'30-may-2018',	'20:20:21', 0,	50.00),
+		(0000000015, 'Fortnite',		'A zebiiiisssss',					40.00,	'IDeal/PayPal', 'Betaal gewoon kill',			'Arnhem',	'Nederland',	7,	'28-may-2018',	'09:20:20.0000000', null,	null,	'Admin',	'Gebruiker1',	'30-may-2018',	'20:20:21', 0,	50.00),
+		(0000000016, 'Battlefield 1',	'Erg leuk',							40.00,	'IDeal/PayPal', 'Betaal gewoon kill',			'Arnhem',	'Nederland',	7,	'28-may-2018',	'09:20:20.0000000', null,	null,	'Admin',	'Gebruiker1',	'30-may-2018',	'20:20:21', 0,	50.00),
+		(0000000017, 'Counter Strike',	'Super pittig',						40.00,	'IDeal/PayPal', 'Betaal gewoon kill',			'Arnhem',	'Nederland',	7,	'28-may-2018',	'09:20:20.0000000', null,	null,	'Admin',	'Gebruiker1',	'30-may-2018',	'20:20:21', 0,	50.00),
+		(0000000018, 'Call Of Duty BO2','BO3 is leuker',					40.00,	'IDeal/PayPal', 'Betaal gewoon kill',			'Arnhem',	'Nederland',	7,	'28-may-2018',	'09:20:20.0000000', null,	null,	'Admin',	'Gebruiker1',	'30-may-2018',	'20:20:21', 0,	50.00);
 
 INSERT INTO Feedback
 (voorwerp,soortgebruiker,feedbacksoort,dag,tijdstip,commentaar)
@@ -3527,7 +3532,12 @@ VALUES	(0000000001,	150.00,	'Gebruiker1','21-apr-2018','12:00:00.0000000'),
 		(0000000005,	88,		'Gebruiker7','28-may-2018','23:04:03.0000000'),
 		(0000000006,	71,		'Gebruiker3','28-may-2018','23:04:03.0000000'),
 		(0000000007,	11,		'Gebruiker5','28-may-2018','23:04:03.0000000'),
-		(0000000008,	271,	'Gebruiker5','28-may-2018','23:04:03.0000000');
+		(0000000008,	271,	'Gebruiker5','28-may-2018','23:04:03.0000000'),
+		(0000000014,	60,		'Gebruiker3','09-feb-2018','16:04:03.0000000'),
+		(0000000015,	70,		'Gebruiker5','28-feb-2018','17:04:03.0000000'),
+		(0000000016,	65,		'Gebruiker7','28-mar-2018','19:04:03.0000000'),
+		(0000000017,	55,		'Gebruiker1','28-jan-2018','21:04:03.0000000'),
+		(0000000018,	130,	'Gebruiker7','28-jan-2018','06:04:03.0000000');
 
 INSERT INTO Bestand
 (filenaam,voorwerp)
@@ -3538,7 +3548,12 @@ VALUES	('bmw304i.jpg',			0000000001),
 		('535i.jpg',			0000000005),
 		('rs6.jpg',				0000000006),
 		('megane.jpg',			0000000007),
-		('c63s.jpg',			0000000008);
+		('c63s.jpg',			0000000008),
+		('bo3.jpg',				0000000014),
+		('fortnite.jpg',		0000000015),
+		('b1.jpg',				0000000016),
+		('cs.jpg',				0000000017),
+		('bo2.jpg',				0000000018),
 
 INSERT INTO VoorwerpInRubriek (voorwerp, rubriekOpLaagsteNiveau, rubriekOpHoogsteNiveau)
 VALUES	(0000000001,9837,	9800),
@@ -3548,4 +3563,9 @@ VALUES	(0000000001,9837,	9800),
 		(0000000005,9837,	9800),
 		(0000000006,9837,	9800),
 		(0000000007,9837,	9800),
-		(0000000008,9837,	9800);
+		(0000000008,9837,	9800),
+		(0000000014,14616,	27234),
+		(0000000015,14616,	27234),
+		(0000000016,14616,	27234),
+		(0000000017,14616,	27234),
+		(0000000018,14616,	27234);
