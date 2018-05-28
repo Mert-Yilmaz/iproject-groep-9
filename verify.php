afe5a3d9a2983c230fc3985907d8133ec60bc2cf
Demivk <38658490+Demivk@users.noreply.github.com>
2018-05-28 15:09:01 +0200

    Update verify.php

--- verify.php
+++ verify.php
@@ -16,21 +16,12 @@ if(isset($_GET['email']) && isset($_GET['code'])) {
     echo "<p>$email, $code</p>";
 
     $query = sqlsrv_query($conn, "SELECT mailbox, code FROM Gebruiker WHERE mailbox='$email' AND code='$code'");
-    $match = sqlsrv_num_rows($query);
-
-    echo $match;
-
-    if($match > 0) {
-        //sqlsrv_query($conn, "UPDATE Gebruiker SET actief=1 WHERE mailbox=$email AND code=$code AND actief=0") OR DIE (sqlsrv_errors());
-        
-        //ANDERS:
+    $queryresult = $dbh->query($query);
+    $count = $queryresult->rowCount();
+    
+    if($count == 1 || $count == -1) {
         $updatestmt = $dbh->prepare("UPDATE Gebruiker SET actief=1 WHERE mailbox='$email' AND code='$code' AND actief=0");
         $updatestmt->execute();
-        
-        //ANDERS:
-        //$update = "UPDATE Gebruiker SET actief=1 WHERE mailbox=? AND code=? AND actief=0";
-        //$update = $dbh->prepare($update);
-        //$update->execute(array($email, $code));
         
         $message = "Bedankt voor het aanmelden! Check je mailbox voor de activatiecode!";
         echo "<h3>$message</h3>";
@@ -38,6 +29,26 @@ if(isset($_GET['email']) && isset($_GET['code'])) {
         $message = "Error, no match.";
         echo "<h3>$message</h3>";
     }
+    
+    /*if($match > 0) {
+        //sqlsrv_query($conn, "UPDATE Gebruiker SET actief=1 WHERE mailbox=$email AND code=$code AND actief=0") OR DIE (sqlsrv_errors());
+
+        //ANDERS:
+        $updatestmt = $dbh->prepare("UPDATE Gebruiker SET actief=1 WHERE mailbox='$email' AND code='$code' AND actief=0");
+        $updatestmt->execute();
+
+        //ANDERS:
+        //$update = "UPDATE Gebruiker SET actief=1 WHERE mailbox=? AND code=? AND actief=0";
+        //$update = $dbh->prepare($update);
+        //$update->execute(array($email, $code));
+
+        $message = "Bedankt voor het aanmelden! Check je mailbox voor de activatiecode!";
+        echo "<h3>$message</h3>";
+    } else {
+        $message = "Error, no match.";
+        echo "<h3>$message</h3>";
+    }*/
+    
 } else {
     echo "<h3>Error, use link</h3>";
 }
