0314cf56d542a3829cdb8ae86198b765ed65f289
Demivk <38658490+Demivk@users.noreply.github.com>
2018-05-30 13:34:27 +0200

    Update verify.php

--- verify.php
+++ verify.php
@@ -10,14 +10,11 @@ include 'functions.php';
 include_once 'db.php';
 error_reporting(E_ALL ^ E_NOTICE);
 
-$connectionInfo = array("DB"=>$dbname, "UN"=>$username, "PW"=>$pw);
-$conn = sqlsrv_connect($hostname, $connectionInfo);
-
 if(isset($_GET['email']) && isset($_GET['code'])) {
     $getemail = $_GET['email'];
     $getcode = $_GET['code'];
     echo "<h3>Email en code meegekregen (check)</h3>";
-    echo "<p>$email, $code</p>";
+    echo "<p>" . $email . ", " . $code . "</p>";
 
     $sqlquery = $dbh->prepare("SELECT mailbox, code FROM Gebruiker WHERE mailbox='$getemail' AND code='$getcode'");
     $sqlquery->setFetchMode(PDO::FETCH_ASSOC);
@@ -30,7 +27,7 @@ if(isset($_GET['email']) && isset($_GET['code'])) {
     $updatestmt = $dbh->prepare("UPDATE Gebruiker SET actief=1 WHERE mailbox='$email' AND code='$code' AND actief=0 AND isToegestaan=1");
     $updatestmt->execute();
 
-    echo "<h3>Bedankt voor het aanmelden! Check je mailbox voor de activatiecode!</h3>";
+    echo "<h3>Bedankt voor het aanmelden!</h3>";
 } else {
     echo "<h3>Error, use link</h3>";
 }
