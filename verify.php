223fc89b062abd10729dd1945b211d45c662dbc7
Demivk <38658490+Demivk@users.noreply.github.com>
2018-05-30 13:56:24 +0200

    Update verify.php

--- verify.php
+++ verify.php
@@ -13,8 +13,6 @@ error_reporting(E_ALL ^ E_NOTICE);
 if(isset($_GET['email']) && !empty($_GET['email']) && isset($_GET['code']) && !empty($_GET['code'])) {
     $getemail = $_GET['email'];
     $getcode = $_GET['code'];
-    echo "<h3>Email en code meegekregen (check)</h3>";
-    echo "<p>" . $getemail . ", " . $getcode . "</p>";
 
     $sqlquery = $dbh->prepare("SELECT mailbox, code FROM Gebruiker WHERE mailbox='$getemail' AND code='$getcode'");
     $sqlquery->setFetchMode(PDO::FETCH_ASSOC);
@@ -27,7 +25,7 @@ if(isset($_GET['email']) && !empty($_GET['email']) && isset($_GET['code']) && !e
     if($getemail=$email && $getcode=$code) {
         $updatestmt = $dbh->prepare("UPDATE Gebruiker SET actief=1 WHERE mailbox='$email' AND code='$code' AND actief=0 AND isToegestaan=1");
         $updatestmt->execute();
-        echo "<h3>Bedankt voor het aanmelden!</h3>";
+        header("Location: index.php");
     }
 } else {
     echo "<h3>Error, use link</h3>";
