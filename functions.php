5fa05ce2369cfe729b9b9843ef50a9913b8abfaf
JeffreyKragten <35327042+JeffreyKragten@users.noreply.github.com>
2018-05-31 11:37:35 +0200

    OY

--- functions.php
+++ functions.php
@@ -43,12 +43,14 @@ function search_item($dbh, $input){
   }
   echo $output;
 }
+zendMailVerloopVeiling($dbh);
 //Hot Items (Selecteert 3 items met de meest recente biedingen)
 function hot_items($dbh){
   $output="";
   $sql = "SELECT TOP (3) * FROM Voorwerp v
           INNER JOIN Bod b on b.voorwerp = v.voorwerpnummer
           INNER JOIN Bestand be on be.voorwerp = v.voorwerpnummer
+          WHERE isToegestaan = 1
           ORDER BY boddag DESC";
   $stmt = $dbh->prepare($sql);
   $stmt->execute();
@@ -67,7 +69,7 @@ function hot_items($dbh){
         <p>Veiling eindigt om: <strong>' . date_format($endtime, "H:i:s") . '</strong> op: <strong>
         ' . date_format($endday, "d/m/Y") . '</strong><p>
         <div class="homepage-items-img">
-            <img src=' . $file . '>
+            <a href="detailpagina.php?item=' . $number . '"><img src=' . $file . '></a>
         </div>
      </div>';
   }
@@ -79,6 +81,7 @@ function ending_items($dbh){
   $sql = "SELECT TOP (3) * FROM Voorwerp v
           INNER JOIN Bod b on b.voorwerp = v.voorwerpnummer
           INNER JOIN Bestand be on be.voorwerp = v.voorwerpnummer
+          WHERE isToegestaan = 1
           ORDER BY looptijdEindeDag, looptijdEindeTijdstip ASC";
   $stmt = $dbh->prepare($sql);
   $stmt->execute();
@@ -97,7 +100,7 @@ function ending_items($dbh){
         <p>Veiling eindigt om: <strong>' . date_format($endtime, "H:i:s") . '</strong> op: <strong>
         ' . date_format($endday, "d/m/Y") . '</strong><p>
         <div class="homepage-items-img">
-            <img src=' . $file . '>
+            ><a href="detailpagina.php?item=' . $number . '"><img src=' . $file . '></a>
         </div>
      </div>';
   }
@@ -109,6 +112,7 @@ function cheap_items($dbh){
   $sql = "SELECT TOP (3) * FROM Voorwerp v
           INNER JOIN Bod b on b.voorwerp = v.voorwerpnummer
           INNER JOIN Bestand be on be.voorwerp = v.voorwerpnummer
+          WHERE isToegestaan = 1
           ORDER BY startprijs-bodbedrag DESC";
   $stmt = $dbh->prepare($sql);
   $stmt->execute();
@@ -127,7 +131,7 @@ function cheap_items($dbh){
         <p>Veiling eindigt om: <strong>' . date_format($endtime, "H:i:s") . '</strong> op: <strong>
         ' . date_format($endday, "d/m/Y") . '</strong><p>
         <div class="homepage-items-img">
-            <img src=' . $file . '>
+            <a href="detailpagina.php?item=' . $number . '"><img src=' . $file . '></a>
         </div>
      </div>';
   }
@@ -195,30 +199,60 @@ function aantalItems($dbh, $rubrieknummer) {
   $query = $dbh->prepare("SELECT COUNT(rubrieknummer) AS Aantal_items
                           FROM	 Rubriek r INNER JOIN VoorwerpInRubriek v
                           ON r.rubrieknummer = v.rubriekOpHoogsteNiveau
-                          WHERE  rubriekOpHoogsteNiveau = :rubrieknummer");
+                          INNER JOIN Voorwerp vv
+                          ON v.voorwerp = vv.voorwerpnummer
+                          WHERE vv.isToegestaan = 1 AND rubriekOpHoogsteNiveau = :rubrieknummer");
   $query->bindParam(':rubrieknummer', $rubrieknummer);
   $query->execute();
   while($row = $query->fetch()) {
       echo $row['Aantal_items'];
   }
 }
+// Haalt het antal items op in een Rubriek
+function aantalItemsSub($dbh, $rubrieknummer, $rubriek) {
+  $query = $dbh->prepare("SELECT COUNT(rubrieknummer) AS Aantal_items
+                          FROM	 Rubriek r INNER JOIN VoorwerpInRubriek v
+                          ON r.rubrieknummer = v.rubriekOpLaagsteNiveau
+                          INNER JOIN Voorwerp vv
+                          ON v.voorwerp = vv.voorwerpnummer
+                          WHERE vv.isToegestaan = 1 AND rubriekOpLaagsteNiveau = :rubrieknummer");
+  $query->bindParam(':rubrieknummer', $rubrieknummer);
+  $query->execute();
+  while($row = $query->fetch()) {
+    if($row['Aantal_items'] != 0){
+      echo $row['Aantal_items'];
+      }
+    }
+  //   $isAantalItemsLeeg = $query->fetchColumn();
+  //     if(!isset($isAantalItemsLeeg)){
+  //       $query = $dbh->prepare("SELECT COUNT(rubrieknummer) AS Aantal_items
+  //                               FROM	 Rubriek r INNER JOIN VoorwerpInRubriek v
+  //                               ON r.rubrieknummer = v.rubriekOpLaagsteNiveau
+  //                               WHERE  rubriekOpLaagsteNiveau = :rubriek");
+  //       $query->bindParam(':rubriek', $rubriek);
+  //       $query->execute();
+  //       while($row = $query->fetch()) {
+  //           echo $row['Aantal_items'];
+  //     }
+  // }
+}
 // Toont PRODUCTEN op producten.php
 function toonItems($dbh, $zoekWoord) {
   try{
-      $query = $dbh->prepare("SELECT *
+      $query = $dbh->prepare("SELECT *, v.voorwerpnummer
                               FROM	Voorwerp v
                               INNER JOIN VoorwerpInRubriek vi
                               ON v.voorwerpnummer = vi.voorwerp
                               INNER JOIN Rubriek r
                               ON r.rubrieknummer = vi.rubriekOpHoogsteNiveau
-                              WHERE r.rubrieknaam
-                              LIKE :zoekwoord
+                              WHERE v.isToegestaan = 1 AND vi.rubriekOpLaagsteNiveau IN (SELECT rubrieknummer
+                                                                  FROM Rubriek
+                                                                  WHERE rubrieknaam LIKE :zoekwoord)
                               ORDER BY v.looptijdEindeTijdstip ASC");
       $query->bindParam(':zoekwoord', $zoekWoord);
       $query->execute();
       while($row = $query->fetch()) {
-          echo "<li>". $row['titel'] . ":
-               ". $row['beschrijving'] . "</li>";
+          echo "<li><a href='detailpagina.php?item=". $row['voorwerpnummer'] . "'><strong>" . $row['titel'] . '</strong></a> ||' . ' startprijs: ' . $row['startprijs'] . '</li>';
       }
   } catch(PDOException $e) {
       echo "Er is iets mis gegaan. De foutmelding is: $e";
@@ -282,9 +316,9 @@ function subCategory($dbh, $rubrieknummer, $rubrieknaam) {
     echo '<li><a href="producten.php?rubriek=' . $row['rubrieknaam'] .
          '&rubriek2=' . $rubrieknaam . '" class="rubrieken">
          ' . $row['rubrieknaam'] . '</a></li>';
+         echo aantalItemsSub($dbh, $row['rubrieknummer'], $row['rubriek']);
   }
 }
-
 // Laat de details van een item zien
 function detailPagina($dbh) {
   $voorwerpnummer = $_GET['item'];
@@ -395,8 +429,6 @@ function detailPagina($dbh) {
     </div>";
   }
 }
-
-
 // Toont details van de 3 hoogste biedingen op het Item
 function biedingenItem($dbh) {
   $voorwerpnummer = $_GET['item'];
@@ -413,19 +445,19 @@ function biedingenItem($dbh) {
     $bodbedrag = $row['bodbedrag'];
     $gebruiker = $row['gebruiker'];
     $boddag = $row['boddag'];
-    $bodtijdstip = $row['bodtijdstip'];
-    echo "$bodbedrag <br>
-          $gebruiker <br>
-          $boddag <br>
-          $bodtijdstip <br> <br> <br>";
+    $bodtijdstip = date_create($row['bodtijdstip']);
+    echo "<ul>
+          <li>Geboden bodbedrag: €$bodbedrag,-</li>
+          <li>Geboden door: $gebruiker<li>
+          <li>Datum: $boddag</li>
+          <li>Tijdstip: " . date_format($bodtijdstip, 'H:i:s'). " </li>
+          </ul>";
   }
 }
-
 // Gebruiker plaatst een bod binnen de richtlijnen
 function biedOpItem($dbh) {
     echo "<form action='#' method='POST'>
-            <label>Bied op dit item!</label>
-            <input type='text' name='bodbedrag' placeholder='bedrag'>
+            <input type='text' name='bodbedrag' placeholder='Vul hier uw gewenste bod in.'>
             <input type='hidden' name='datum' value=" . date("m/d/Y") . ">
             <input type='hidden' name='tijd' value=" . date("H:i") . ">
             <input type='submit' class='knop' value='Bied' name='submit'>
@@ -510,6 +542,224 @@ function biedOpItem($dbh) {
         echo '<script type="text/javascript">alert("Bedrag te laag!")</script>';
     }
   }
+}
+//Versturen van een mail (1 dag voor verloopdatum)
+function zendMailVerloopVeiling($dbh) {
+    if(isset($_SESSION['login-token'])) {
+        $logintoken = $_SESSION['login-token'];
+        $sqlquery = $dbh->prepare("SELECT gebruikersnaam FROM Gebruiker WHERE gebruikersnaam = '$logintoken' OR mailbox = '$logintoken'");
+        $sqlquery->setFetchMode(PDO::FETCH_ASSOC);
+        $sqlquery->execute();
+        $sessionQueryData = $sqlquery->fetch();
+        $sessionGebruikersnaam = $sessionQueryData['gebruikersnaam'];
+        $getGebruikersnaamQuery = $dbh->prepare("SELECT gebruikersnaam FROM Gebruiker WHERE gebruikersnaam = '$sessionGebruikersnaam'");
+        $getGebruikersnaamQuery->setFetchMode(PDO::FETCH_ASSOC);
+        $getGebruikersnaamQuery->execute();
+        $getGebruikersnaamData = $getGebruikersnaamQuery->fetch();
+        $getGebruikersnaam = $getGebruikersnaamData['gebruikersnaam'];
+        $query = $dbh->prepare("SELECT V.voorwerpnummer, V.titel, V.looptijdEindeDag, V.looptijdEindeTijdstip, V.isMailVerstuurd, G.mailbox
+                                 FROM Voorwerp V INNER JOIN Gebruiker G ON v.verkoper = G.gebruikersnaam
+                                 WHERE V.verkoper='$getGebruikersnaam'");
+        $query->setFetchMode(PDO::FETCH_ASSOC);
+        $query->execute();
+        $data = $query->fetch();
+        $voorwerpnummer = $data['voorwerpnummer'];
+        $titel = $data['titel'];
+        $enddate = $data['looptijdEindeDag'];
+        $endtime = $data['looptijdEindeTijdstip'];
+        $isMailVerstuurd = $data['isMailVerstuurd'];
+        $email = $data['mailbox'];
+        $day = date('d', strtotime($enddate));
+        $month = date('m', strtotime($enddate));
+        $year = date('Y', strtotime($enddate));
+        $hour = date('H', strtotime($endtime));
+        $minute = date('i', strtotime(($endtime)));
+        $seconds = date('s', strtotime($endtime));
+        $from_unix_time = mktime($hour, $minute, $seconds, $month, $day, $year);
+        $day_before = strtotime("yesterday", $from_unix_time);
+        $formatted = date("Y-m-d", $day_before);
+        $today = date("Y-m-d");
+        if ($formatted >= $today && $isMailVerstuurd == 0) {
+            $to = $email;
+            $from = 'noreply@eenmaalandermaal9.nl';
+            $subject = 'Uw veiling verloopt morgen!';
+            $message = '
+            Beste ' . $getGebruikersnaam . ',
+            Je veiling ' . $titel . ' verloopt morgen!';
+            $headers = 'From: ' . $from . "\r\n";
+            mail($to, $subject, $message, $headers);
+            $sql = $dbh->prepare("UPDATE Voorwerp
+                                   SET isMailVerstuurd=1
+                                   WHERE voorwerpnummer=$voorwerpnummer");
+            $sql->setFetchMode(PDO::FETCH_ASSOC);
+            $sql->execute();
+        }
+    }
+}
+zendMailVerloopVeiling($dbh);
+
+
+function plaatsItem($dbh) {
+  $forms = '';
+  $forms .= '
+        <form action="#" method="post" enctype="multipart/form-data">
+        <div>
+           <label>Naam van het Product</label>
+           <input type="text" name="titel" placeholder="Titel" maxlength="18">
+         </div>';
+         $forms .= "<label>Hoofdrubriek<label>
+                    <select name = rij1>";
+         $query = $dbh->prepare("SELECT * FROM Rubriek WHERE rubriek = -1");
+         $query->execute();
+         while($row = $query->fetch()) {
+             $forms .= "<option value = ". $row['rubrieknummer'] .">" . $row['rubrieknaam'] . "</option>";
+         }
+         $forms .= "</select>";
+         $forms .= '<div>
+          <label>Voeg een kleine beschrijving toe</label>
+          <textarea rows="4" name="beschrijving" placeholder="Geef hier de beschrijving van het product.. (optioneel)" maxlength="22" required></textarea>
+        </div>
+        <div>
+          <label>Prijs in: €</label>
+          <input type="text" name="startprijs" placeholder="99999.99 (Startprijs)" maxlength="8" required>
+        </div>
+        <div>
+          <label>Hoe wil je worden betaald?</label>
+          <select name="betalingswijze">
+            <option value disabled selected>Kies een betalingswijze</option>
+            <option value="IDeal/PayPal">IDeal/Paypal</option>
+            <option value="Creditcard">Creditcard</option>
+          </select>
+        </div>
+        <div>
+          <label>Geef hier nog wat extra betaalinstructies</label>
+          <input type="text" name="betalingsinstructie" placeholder="Geef hier aanvullende betaal instructies" maxlength="30">
+        </div>
+        <div>
+          <label>Waar wil je het item aanbieden?</label>
+          <input type="text" name="plaats" placeholder="Plaats naam" maxlength="12">
+        </div>
+        <div>
+          <label>In welk land wil je het item aanbieden?</label>
+          <select name="land">
+            <option value disabled selected>Selecteer een land</option>
+            <option value = "Afghanistan"> Afghanistan </option>
+            <option value = "Albania"> Albanië </option>
+            <option value = "Algerije"> Algerije </option>
+            <option value = "Amerikaans Samoa"> Amerikaans Samoa </option>
+            <option value = "Andorra"> Andorra </option>
+            <option value = "Angola"> Angola </option>
+            <option value = "Anguilla"> Anguilla </option>
+            <option value = "Antartica"> Antarctica </option>
+            <option value = "Antigua en Barbuda"> Antigua en Barbuda </option>
+            <option value = "Argentina"> Argentinië </option>
+            <option value = "Armenia"> Armenië </option>
+            <option value = "Aruba"> Aruba </option>
+            <option value = "Nederland"> Nederland </option>
+            <option value = "Czech Republic"> Czech Republic </option>
+            <option value = "Denemarken"> Denemarken </option>
+            <option value = "Djibouti"> Djibouti </option>
+            <option value = "Dominica"> Dominica </option>
+            <option value = "Guatemala"> Guatemala </option>
+            <option value = "Guinea"> Guinea </option>
+            <option value = "Guinee-Bissau"> Guinee-Bissau </option>
+            <option value = "Guyana"> Guyana </option>
+            <option value = "Haiti"> Haïti </option>
+            <option value = "Heilige Stoel"> Heilige Stoel (Vaticaanstad) </option>
+            <option value = "Honduras"> Honduras </option>
+            <option value = "Anders"> Overig </option>
+          </select>
+        </div>
+        <div>
+          <label>Hoe lang wil je het product aanbieden?</label>
+          <select name="looptijd">
+            <option value selected disabled>Selecteer de looptijd in dagen</option>
+            <option value = "3"> 1 </option>
+            <option value = "3"> 3 </option>
+            <option value = "5"> 5 </option>
+            <option value = "7" selected="selected"> 7 (Standaard) </option>
+            <option value = "10"> 10 </option>
+          </select>
+        </div>
+        <div>
+          <label>Hoe hoog zijn de verzendkosten</label>
+          <input type="text" name="verzendkosten" placeholder="99.99 (Verzendkosten)" maxlength="5">
+        </div>
+        <div>
+          <label>Eventuele verdere verzendinstructies?</label>
+          <textarea rows="4" name="verzendinstructies" placeholder="Geef hier instructies voor het verzenden.. (optioneel)" maxlength="30"></textarea>
+        </div>
+        <div>
+          <label>Upload hier je image</label>
+          <input name="plaatje" type="file" accept=".jpg, .jpeg, .bpem, .png" required>
+        </div>
+        <div>
+          <label>Upload hier je image</label>
+          <input name="plaatje2" type="file" accept=".jpg, .jpeg, .bpem, .png">
+        </div>
+        <div>
+          <label>Upload hier je image</label>
+          <input name="plaatje3" type="file" accept=".jpg, .jpeg, .bpem, .png">
+        </div>
+        <div>
+          <button type="submit" class="knop" name="submit">Plaats</button>
+        </div>
+        <input type="hidden" name="time" value="' . date("H:i:s") . '">
+      </form>
+      </div>
+      <div class="medium-3 large-4"></div>';
+    echo $forms;
+    if (isset($_POST["submit"])){
+      $target_dir = "img/veilingen/";
+      $target_file = $target_dir . basename($_FILES["plaatje"]["name"]);
+      $uploadOk = 1;
+      $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+        $begindag = date("Y/m/d");// Deze drie dingen nog verbeteren
+        $einddag = date("Y-M-D");
+        $plaatstijd = $_POST['time'];
+        $veilinggesloten = 0;
+        is_null($verkoopprijs);
+
+        $titel = $_POST["titel"];
+        $beschrijving = $_POST["beschrijving"];
+        $startprijs = $_POST["startprijs"];
+        $betalingswijze = $_POST["betalingswijze"];
+        $betalingsinstructie = $_POST["betalingsinstructie"];
+        $plaats = $_POST["plaats"];
+        $land = $_POST["land"];
+        $looptijd = $_POST["looptijd"];
+        $verzendkosten = $_POST["verzendkosten"];
+        $verzendinstructies = $_POST["verzendinstructies"];
+        $plaatje = $_POST["plaatje"];
+        $hoogste = $_POST["rij1"];
+        $koper = 'Gebruiker5'; // Dit nog aanpassen naar session
+
+        //query om het voorwerpnummer te bepalen
+        $nRows = $dbh->query("SELECT count(*) FROM Voorwerp")->fetchColumn();
+        $voorwerpid = 1 + $nRows;
+
+        try{
+        $query = $dbh->prepare("INSERT INTO Voorwerp
+                              	VALUES ('$voorwerpid','$titel','$beschrijving',
+                                         '$startprijs','$betalingswijze','$betalingsinstructie',
+                                         '$plaats','$land','$looptijd',
+                                         '1-1-2018','12:00:00','$verzendkosten',
+                                         '$verzendinstructies','Admin','$koper',
+                                         '2-1-2018','12:00:01',0,NULL, 1, 0)");
+          $query->execute();
+        }catch(PDOException $e) {
+          echo '<script type="text/javascript">alert("Gegevens niet goed ingevuld")</script>';
+        }
+        try{
+        $query2 = $dbh->prepare("INSERT INTO VoorwerpInRubriek
+                              	 VALUES ('$voorwerpid','$hoogste','$hoogste')");
+        $query2->execute();
+        }catch(PDOException $e) {
+          echo '<script type="text/javascript">alert("Gegevens niet goed ingevuld")</script>';
+        }
+        move_uploaded_file($_FILES["plaatje"]["tmp_name"], $target_file);
+    }
 }
 
 
