366ce8d3f319186de3ab65f5d044cb04187d3505
MNLBHAN <38658425+MNLBHAN@users.noreply.github.com>
2018-05-31 14:31:21 +0200

    indent

--- register.php
+++ register.php
@@ -1,18 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: demiv
- * Date: 28-5-2018
- * Time: 10:56
- */
-
-session_start();
-include 'functions.php';
-include_once 'db.php';
+require "functions.php";
+require "db.php";
 error_reporting(E_ALL ^ E_NOTICE);
 ?>
-
-
 <!doctype html>
 <html class="no-js" lang="en" dir="ltr">
 <head>
@@ -24,107 +14,103 @@ error_reporting(E_ALL ^ E_NOTICE);
     <link rel="stylesheet" href="css/app.css">
     <link rel="icon" href="img/logo2.PNG">
 </head>
-
-
 <body>
-<?php include 'navbar.php'; ?>
-<div class="grid-x">
-    <div class="medium-3"></div>
-    <div class="medium-6 text-center">
-        <h1 class=text-center>Accountregistratie</h1>
+    <?php require "navbar.php"; ?>
+    <div class="grid-x">
+        <div class="medium-3"></div>
+            <div class="medium-6 text-center">
+                <h1 class="text-center">Accountregistratie</h1>
+            </div>
+        <div class="medium-3"></div>
     </div>
-    <div class="medium-3"></div>
-</div>
-<div class="grid-x">
-  <div class="large-4 medium-3"></div>
-    <div class="medium-6 small-12 large-4">
-        <form class="registerform" method="POST" action="register.php">
-            <div>
-                <input type="text" name="Naam" placeholder="Voornaam, verplicht" maxlength="15" required>
-            </div>
-            <div>
-                <input type="text" name="Achternaam" placeholder="Achternaam, verplicht" maxlength="20" required>
-            </div>
-            <div>
-                <label>Geboortedatum</label>
-                <input type="date" name="Geboortedatum" placeholder="Geboortedatum, verplicht" required>
-            </div>
-            <div>
-                <input type="email" name="Email" placeholder="E-Mail, verplicht" maxlength="50" required>
-            </div>
-            <div>
-                <input type="text" name="Gebruikersnaam" placeholder="Gebruikersnaam, verplicht" maxlength="20" required>
-            </div>
-            <div>
-                <input type="password" name="Wachtwoord" placeholder="Wachtwoord (Minimaal 7 tekens, een hoofdletter en een getal), verplicht" maxlength="100"
-                        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}" required>
-            </div>
-            <div>
-                <input type="text" name="Adresregel1" placeholder="Adres, verplicht" maxlength="30" required>
-            </div>
-            <div>
-                <input type="text" name="Plaats" placeholder="Plaats, verplicht" maxlength="15" required>
-            </div>
-            <div>
-                <input type="text" name="Postcode" placeholder="Postcode, verplicht" maxlength="7" required>
-            </div>
-            <div>
-                <select name="Land">
-                    <option value disabled selected>Selecteer een land, verplicht</option>
-                    <option value="Afghanistan"> Afghanistan</option>
-                    <option value="Albania"> Albanië</option>
-                    <option value="Algerije"> Algerije</option>
-                    <option value="Amerikaans Samoa"> Amerikaans Samoa</option>
-                    <option value="Andorra"> Andorra</option>
-                    <option value="Angola"> Angola</option>
-                    <option value="Anguilla"> Anguilla</option>
-                    <option value="Antartica"> Antarctica</option>
-                    <option value="Antigua en Barbuda"> Antigua en Barbuda</option>
-                    <option value="Argentina"> Argentinië</option>
-                    <option value="Armenia"> Armenië</option>
-                    <option value="Aruba"> Aruba</option>
-                    <option value="Nederland"> Nederland</option>
-                    <option value="Czech Republic"> Czech Republic</option>
-                    <option value="Denemarken"> Denemarken</option>
-                    <option value="Djibouti"> Djibouti</option>
-                    <option value="Dominica"> Dominica</option>
-                    <option value="Guatemala"> Guatemala</option>
-                    <option value="Guinea"> Guinea</option>
-                    <option value="Guinee-Bissau"> Guinee-Bissau</option>
-                    <option value="Guyana"> Guyana</option>
-                    <option value="Haiti"> Haïti</option>
-                    <option value="Heilige Stoel"> Heilige Stoel (Vaticaanstad)</option>
-                    <option value="Honduras"> Honduras</option>
-                    <option value="Turkije"> Turkije</option>
-                    <option value="Anders"> Overig</option>
-                </select>
-            </div>
-            <div>
-                <input type="text" name="Adresregel2" maxlength="15" placeholder="Adres 2, niet verplicht">
-            </div>
-            <div>
-                <select name="Vraag"> <!-- Ophalen uit DB? -->
-                    <option value disabled selected>Selecteer een beveiligingsvraag, verplicht</option>
-                    <option value="1">Welk gerecht kon je als eerste koken?</option>
-                    <option value="2">Wat is je moeders tweede naam?</option>
-                    <option value="3">Welke bijnaam had je als kind?</option>
-                    <option value="4">In welke plaats ben je geboren?</option>
-                    <option value="5">Hoe heette je eerste huisdier?</option>
-                </select>
-            </div>
-            <div>
-                <input type="text" name="Antwoord" placeholder="Antwoord beveiligingsvraag, verplicht" maxlength="20" required>
-            </div>
-            <div>
-                <button type="submit" class="knop" name="reg_user">Registreer</button>
-            </div>
-            <p>
-                Heb je al een account? <a href="login-page.php">Klik hier om in te loggen</a>
-            </p>
-        </form>
+    <div class="grid-x">
+        <div class="large-4 medium-3"></div>
+            <div class="medium-6 small-12 large-4">
+                <form class="registerform" method="POST" action="register.php">
+                    <div>
+                        <input type="text" name="Naam" placeholder="Voornaam, verplicht" maxlength="15" required>
+                    </div>
+                    <div>
+                        <input type="text" name="Achternaam" placeholder="Achternaam, verplicht" maxlength="20" required>
+                    </div>
+                    <div>
+                        <label>Geboortedatum</label>
+                        <input type="date" name="Geboortedatum" placeholder="Geboortedatum, verplicht" required>
+                    </div>
+                    <div>
+                        <input type="email" name="Email" placeholder="E-Mail, verplicht" maxlength="50" required>
+                    </div>
+                    <div>
+                        <input type="text" name="Gebruikersnaam" placeholder="Gebruikersnaam, verplicht" maxlength="20" required>
+                    </div>
+                    <div>
+                        <input type="password" name="Wachtwoord" placeholder="Wachtwoord (Minimaal 7 tekens, een hoofdletter en een getal), verplicht" maxlength="100"
+                            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}" required>
+                    </div>
+                    <div>
+                        <input type="text" name="Adresregel1" placeholder="Adres, verplicht" maxlength="30" required>
+                    </div>
+                    <div>
+                        <input type="text" name="Plaats" placeholder="Plaats, verplicht" maxlength="15" required>
+                    </div>
+                    <div>
+                        <input type="text" name="Postcode" placeholder="Postcode, verplicht" maxlength="7" required>
+                    </div>
+                    <div>
+                        <select name="Land">
+                            <option value disabled selected>Selecteer een land, verplicht</option>
+                            <option value="Afghanistan"> Afghanistan</option>
+                            <option value="Albania"> Albanië</option>
+                            <option value="Algerije"> Algerije</option>
+                            <option value="Amerikaans Samoa"> Amerikaans Samoa</option>
+                            <option value="Andorra"> Andorra</option>
+                            <option value="Angola"> Angola</option>
+                            <option value="Anguilla"> Anguilla</option>
+                            <option value="Antartica"> Antarctica</option>
+                            <option value="Antigua en Barbuda"> Antigua en Barbuda</option>
+                            <option value="Argentina"> Argentinië</option>
+                            <option value="Armenia"> Armenië</option>
+                            <option value="Aruba"> Aruba</option>
+                            <option value="Nederland"> Nederland</option>
+                            <option value="Czech Republic"> Czech Republic</option>
+                            <option value="Denemarken"> Denemarken</option>
+                            <option value="Djibouti"> Djibouti</option>
+                            <option value="Dominica"> Dominica</option>
+                            <option value="Guatemala"> Guatemala</option>
+                            <option value="Guinea"> Guinea</option>
+                            <option value="Guinee-Bissau"> Guinee-Bissau</option>
+                            <option value="Guyana"> Guyana</option>
+                            <option value="Haiti"> Haïti</option>
+                            <option value="Heilige Stoel"> Heilige Stoel (Vaticaanstad)</option>
+                            <option value="Honduras"> Honduras</option>
+                            <option value="Turkije"> Turkije</option>
+                            <option value="Anders"> Overig</option>
+                        </select>
+                    </div>
+                    <div>
+                        <input type="text" name="Adresregel2" maxlength="15" placeholder="Adres 2, niet verplicht">
+                    </div>
+                    <div>
+                        <select name="Vraag">
+                            <option value disabled selected>Selecteer een beveiligingsvraag, verplicht</option>
+                            <option value="1">Welk gerecht kon je als eerste koken?</option>
+                            <option value="2">Wat is je moeders tweede naam?</option>
+                            <option value="3">Welke bijnaam had je als kind?</option>
+                            <option value="4">In welke plaats ben je geboren?</option>
+                            <option value="5">Hoe heette je eerste huisdier?</option>
+                        </select>
+                    </div>
+                    <div>
+                        <input type="text" name="Antwoord" placeholder="Antwoord beveiligingsvraag, verplicht" maxlength="20" required>
+                    </div>
+                    <div>
+                        <button type="submit" class="knop" name="reg_user">Registreer</button>
+                    </div>
+                    <p>
+                        Heb je al een account? <a href="login-page.php">Klik hier om in te loggen</a>
+                    </p>
+                </form>
     </div>
-    <!--<div class="medium-3 large-4"></div>-->
-
     <?php
     if (isset($_POST['reg_user'])) {
         $gebruikersnaam = $_POST["Gebruikersnaam"];
@@ -146,19 +132,15 @@ error_reporting(E_ALL ^ E_NOTICE);
         $isToegestaan = 1;
         $verkopercode = NULL;
 
-        //Prepare
         $query = "INSERT INTO Gebruiker VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
         $query = $dbh->prepare($query);
 
-        //In DB
         try {
             $query->execute(array($gebruikersnaam, $voornaam, $achternaam, $adresregel1, $adresregel2, $postcode, $plaats, $land, $geboortedatum, $email, $wachtwoord, $vraag, $antwoord, $verkoper, $code, $actief, $isToegestaan, $verkopercode));
-
             $to      = $email;
             $from    = 'noreply@eenmaalandermaal9.nl';
             $subject = 'Verificatie account EenmaalAndermaal';
             $message = '
-
 
             Beste ' . $voornaam . ',
             Bedankt voor het aanmelden op EenmaalAndermaal!
@@ -173,12 +155,12 @@ error_reporting(E_ALL ^ E_NOTICE);
 
             $headers = 'From: ' . $from . "\r\n";
             mail($to, $subject, $message, $headers);
-        } catch (PDOException $e) {
-            echo $e;
-            echo '<script type="text/javascript">alert("Gegevens niet goed ingevuld")</script>';
-        }
-    }
-    include("footer.html");
+            } catch (PDOException $e) {
+                    echo $e;
+                    echo '<script type="text/javascript">alert("Gegevens niet goed ingevuld")</script>';
+                  }
+                }
+            require "footer.html";
     ?>
 </body>
 </html>
