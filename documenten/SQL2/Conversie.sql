88f12f424f014abaffb3f40a1079f5ad15f559bf
Demivk <38658490+Demivk@users.noreply.github.com>
2018-06-09 12:52:43 +0200

    Add files via upload

--- documenten/SQL2/Conversie.sql
+++ documenten/SQL2/Conversie.sql
@@ -60,7 +60,11 @@ FROM iproject9.dbo.Items
 INSERT INTO iproject9.dbo.VoorwerpInRubriek(voorwerp, rubriekOpLaagsteNiveau, rubriekOpHoogsteNiveau)
 SELECT	CAST(ID AS NUMERIC(20)) AS voorwerp,
 		Categorie AS rubriekOpLaagsteNiveau,
-		rubriekOpHoogsteNiveau = -1
+		rubriekOpHoogsteNiveau = 1188
+		-- Hiervoor dient nog een functie geschreven te worden die de hoofdcategorie ophaalt, om zo de breadcrumbs te laten werken.
+		-- Omdat onze externe populatie zich op dit moment bevindt in de categorie 'Modelbouw en miniaturen',
+		-- is de hoogste rubriek 1188
+		-- Kan ook op -1 (Root), maar worden dan niet getoond achter de hoofdrubriek.
 FROM iproject9.dbo.Items
 
 GO
