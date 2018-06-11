a9567a5895ffd9a2cf47ad0eff59275821c770dd
Demivk <38658490+Demivk@users.noreply.github.com>
2018-06-11 14:07:45 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@
      "$username", "$password");
      // set the PDO error mode to exception
      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-     //return $dbh;
+     return $dbh;
      }
  catch(PDOException $e)
      {
@@ -1262,40 +1262,4 @@ function biedOpItem($dbh) {
      }
    }
  }
-       
-//  function sluitVeilingen($dbh) {
-//     date_default_timezone_get("Europe/Amsterdam");
-
-//     $query = $dbh->prepare("SELECT * FROM Voorwerp");
-//     $query->setFetchMode(PDO::FETCH_ASSOC);
-//     $query->execute();
-
-//     while($data = $query->fetch()) {
-//         $itemID = $data['voorwerpnummer'];
-//         $enddate = $data['looptijdEindeDag'];
-//         $endtime = $data['looptijdEindeTijdstip'];
-//         $veilingGesloten = $data['veilingGesloten']; //0 = false, 1 = true
-
-//         $day = date('d', strtotime($enddate));
-//         $month = date('m', strtotime($enddate));
-//         $year = date('Y', strtotime($enddate));
-//         $hour = date('H', strtotime($endtime));
-//         $minute = date('i', strtotime(($endtime)));
-//         $seconds = date('s', strtotime($endtime));
-//         $from_unix_time = mktime($hour, $minute, $seconds, $month, $day, $year);
-
-//         $formattedDate = date("Y-m-d", $from_unix_time);    //DB
-//         $formattedTime = date("H:i:s", $from_unix_time);    //DB
-//         $todayDate = date("Y-m-d");                         //today
-//         $todayTime = date("H:i:s");                         //today
-
-//         if((($todayDate == $formattedDate && $todayTime >= $formattedTime) || $todayDate > $formattedDate) && $veilingGesloten == 0){
-//             //DB date is vandaag en tijd > vandaag, of de date is al geweest.
-//             $update = $dbh->prepare("UPDATE Voorwerp SET veilingGesloten = 1 WHERE voorwerpnummer = '$itemID'");
-//             $update->execute();
-//         }
-//     }
-// }
-       
-//        sluitVeilingen($dbh);
  ?>
